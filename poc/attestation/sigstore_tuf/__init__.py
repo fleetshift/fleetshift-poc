d6b450fda88f36608dacd79c7bf6d2e4a7db474f
Alec Henninger <alechenninger@gmail.com>
2026-08-18 17:09:28 -0400

    Remove superseded sigstore poc

--- poc/attestation/sigstore_tuf/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Sigstore + TUF attestation POC (Mode A: Fulcio + TSA, no transparency log).
-
-See README.md for architecture and how this relates to ``hybrid/``.
-"""
-
-from .verify import verify_delivery
-
-__all__ = ["verify_delivery"]
